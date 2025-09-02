1d0aa298f957b6646b4ad85d13b412b542b67672
kaooat7050-boop <kaooat.7050@gmail.com>
2025-09-02 10:36:15 +0700

    Loop

---
+++ pt024_0711_Loop.py
@@ -0,0 +1,15 @@
+# loop thronugh string
+site = 'marcuscode'
+for n in site:
+    print(n)
+
+# loop thronugh list
+names = ['Mateo', 'John', 'Eric', 'Mark', 'Robert']
+for n in names:
+    print(n)
+
+
+    numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+    for n in numbers:
+        print(n)
+        
